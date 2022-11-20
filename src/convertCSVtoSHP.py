379c259ae415c7830cd71a2dc7578e88bb19ebb1
Moksha <moksha.srinivas@gmail.com>
2022-11-19 23:58:49 -0500

    fixes #33 TODO: - add doxygen style comments - add shp to network dataset functionality - add more lat/long csv testcases

--- src/convertCSVtoSHP.py
+++ src/convertCSVtoSHP.py
@@ -9,27 +9,27 @@ and convert them to point SHP files
 
 import pandas as pd
 import geopandas as gpd
-import matplotlib.pyplot as plt
 import os
 from os.path import join
 
 
 def readDir(inputpath):
-    files = os.listdir(inputpath)
-    return files
+    return [os.path.join(inputpath, file) for file in os.listdir(inputpath)]
 
 
 def convertCSVtoSHP(filenames):
-    curDir =  os.getcwd()
-    targetPath = os.path.join(curDir, "/SHPfiles/")
+    targetPath = os.path.join("../", "SHPfiles/")
     os.mkdir(targetPath)
+    print("Here is the list of tiles in the directory: ", filenames)
+    print("Print the targetpath: ", targetPath)
+    os.chdir(targetPath)
     for csvfile in range(len(filenames)):
-        data = pd.read_csv(csvfile)
-        dataGDF = gpd.GeoDataFrame(data, geometry = gpd.points_from_xy(data['longitude'], data['latitude']))
+        data = pd.read_csv(filenames[csvfile])
+        dataGDF = gpd.GeoDataFrame(data, geometry = gpd.points_from_xy(data['Longitude'], data['Latitude']))
+        print("We have gotten to this point")
         ESRI_WKT = 'GEOGCS["GCS_WGS_1984",DATUM["D_WGS_1984",SPHEROID["WGS_1984",6378137,298.257223563]],PRIMEM["Greenwich",0],UNIT["Degree",0.017453292519943295]]'
-        shpfilename = 'travelepisode' + csvfile + ".shp"
-        dataGDF.to_file(filename=shpfilename, driver = 'ESRI Shapefile', crs_wkt='ESRI_WKT')
-
+        shpfilename = 'travelepisode' + str(csvfile) + ".shp"
+        dataGDF.to_file(filename=shpfilename, driver = 'ESRI Shapefile', crs=ESRI_WKT)
     return targetPath
 
 
