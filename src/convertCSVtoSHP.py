d24d6703537e1d4d60cc5071f57c5c0c5a105f4c
Moksha <moksha.srinivas@gmail.com>
2022-11-23 17:01:21 -0500

    Fixes #33 -addressed review comments

--- src/convertCSVtoSHP.py
+++ src/convertCSVtoSHP.py
@@ -19,15 +19,17 @@ def readDir(inputpath):
     return [os.path.join(inputpath, file) for file in os.listdir(inputpath)]
 
 
-#The function receives an array of csv epsiode files and 
-#returns a directory filled with the corresponding point shapefiles 
+#The function receives an array of csv episode filepaths and 
+#returns a directory with the corresponding point shapefiles 
 def convertCSVtoSHP(filenames):
     targetPath = os.path.join("../", "SHPfiles/")
     os.mkdir(targetPath)
     os.chdir(targetPath)
+    #Loops through each csvfile, creates point SHP file and writes to the new directory
     for csvfile in range(len(filenames)):
         data = pd.read_csv(filenames[csvfile])
         dataGDF = gpd.GeoDataFrame(data, geometry = gpd.points_from_xy(data['Longitude'], data['Latitude']))
+        #Standardized ESRI wkt projection
         ESRI_WKT = 'GEOGCS["GCS_WGS_1984",DATUM["D_WGS_1984",SPHEROID["WGS_1984",6378137,298.257223563]],PRIMEM["Greenwich",0],UNIT["Degree",0.017453292519943295]]'
         shpfilename = 'travelepisode' + str(csvfile) + ".shp"
         dataGDF.to_file(filename=shpfilename, driver = 'ESRI Shapefile', crs=ESRI_WKT)
