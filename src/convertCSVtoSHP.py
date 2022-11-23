034bb99660ae171d0686dc46165b8060e0c6e745
Moksha <moksha.srinivas@gmail.com>
2022-11-23 16:42:59 -0500

    Fixes #33 - removed testing print statements

--- src/convertCSVtoSHP.py
+++ src/convertCSVtoSHP.py
@@ -24,13 +24,10 @@ def readDir(inputpath):
 def convertCSVtoSHP(filenames):
     targetPath = os.path.join("../", "SHPfiles/")
     os.mkdir(targetPath)
-    print("Here is the list of tiles in the directory: ", filenames)
-    print("Print the targetpath: ", targetPath)
     os.chdir(targetPath)
     for csvfile in range(len(filenames)):
         data = pd.read_csv(filenames[csvfile])
         dataGDF = gpd.GeoDataFrame(data, geometry = gpd.points_from_xy(data['Longitude'], data['Latitude']))
-        print("We have gotten to this point")
         ESRI_WKT = 'GEOGCS["GCS_WGS_1984",DATUM["D_WGS_1984",SPHEROID["WGS_1984",6378137,298.257223563]],PRIMEM["Greenwich",0],UNIT["Degree",0.017453292519943295]]'
         shpfilename = 'travelepisode' + str(csvfile) + ".shp"
         dataGDF.to_file(filename=shpfilename, driver = 'ESRI Shapefile', crs=ESRI_WKT)
