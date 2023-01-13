e8563c212a200b52c57150fca63910cf3a4ab9aa
smita7 <smitadsingh4u@gmail.com>
2023-01-12 23:50:28 -0500

    code can now fetch all potential activity locations for a individual stop episode

--- src/fetchActivityLocations.py
+++ src/fetchActivityLocations.py
@@ -6,6 +6,8 @@ import requests
 def fetchActivityLocations(longitude, latitude):
     tolerance = 25 #tolerance of the surrounding areas in meters
     long_lat_tol_query_str = str(tolerance)+''',''' + str(longitude) +''','''+ str(latitude)
+    activityLocationList = []
+    activityLocationIndex = []
     built_query = '''
     [out:json][timeout:25];
     // gather results
@@ -23,7 +25,6 @@ def fetchActivityLocations(longitude, latitude):
     api = overpy.Overpass()                       # creating a overpass API instance 
     result = api.query(built_query)               # get result from API by sending the query to overpass servers
 
-
     list_of_node_tags = []                        # initializing empty list , we'll use it to form a dataframe .
     for node in result.nodes:                     # from each node , get the all tags information
         node.tags['latitude'] =  node.lat
@@ -36,6 +37,24 @@ def fetchActivityLocations(longitude, latitude):
         print(column_name)
         if "name" not in column_name:
             data_frame.drop(column_name, axis=1, inplace=True)
+    
+    if "name" in data_frame.columns:
+        for row in data_frame.itertuples():
+            if str(row.name) != "nan":
+                print(row.name)
+                activityLocationList.append(str(row.name))
+                activityLocationIndex.append(row.Index)
+                data_frame.drop(row.Index, inplace=True)
+        data_frame.drop("name", axis=1, inplace=True)
+
+    for index in range(len(data_frame.columns)):
+        for row in data_frame.itertuples():
+            if str(row[index+1]) != "nan" and row.Index not in activityLocationIndex:
+                activityLocationList.append(str(row[index+1]))
+                activityLocationIndex.append(row.Index)
+                data_frame.drop(row.Index, inplace=True)
+
+    print(activityLocationList)
     print(data_frame)                              # return data frame if you want to use it further in main function.
 
 
