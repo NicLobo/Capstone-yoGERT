8f855e5d3a41e0940144eeaae5bd013715d5f877
Niyatha Rangarajan <59780995+rangaran@users.noreply.github.com>
2023-01-30 11:19:14 -0500

    fixed str to float

--- src/Transformation.py
+++ src/Transformation.py
@@ -1,18 +1,24 @@
 import csv
-# read csv file as a list of lists
 
-def GenInput(filepath):
+def convert_to_floats(arr):
+    result = map(float, arr)
+    return list(result)
+
+def GenALInput(filepath):
     data = csv.reader(open(filepath))
 
     li = []
     c=0
+    
     for line in data:
         
         if c>0: 
-            li.append(tuple(line))
+            li.append(tuple(convert_to_floats(line[2:4])))
         
         c = c+1
-    
+
     return li
+    
 
-
+l = GenALInput('./episode.csv')
+print(l)
