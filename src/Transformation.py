3163ebfa355dbd3aa857130608b9f33779767aed
Niyatha Rangarajan <59780995+rangaran@users.noreply.github.com>
2023-02-01 15:19:44 -0500

    Added datetime string list

--- src/Transformation.py
+++ src/Transformation.py
@@ -1,4 +1,5 @@
 import csv
+from datetime import datetime
 
 def convert_to_floats(arr):
     result = map(float, arr)
@@ -23,19 +24,23 @@ def GenALInputT(filepath):
     data = csv.reader(open(filepath))
 
     li = []
+    lt = []
     c=0
     
     for line in data:
         
         if c>0: 
-            li.append(tuple(convert_to_floats(line[2:6])))
+            li.append(tuple(convert_to_floats(line[2:4])))
+            lt.append(line[5])
+
         
         c = c+1
 
-    return li
+    return li,lt
     
 
 l = GenALInput('./episode.csv')
-l2 = GenALInputT('./episode.csv')
+l2,ltime = GenALInputT('./episode.csv')
 print(l)
 print(l2)
+print(ltime)
