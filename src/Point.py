633951281198d73127cf92cd88e116f4baf3ca82
Abeer Al-Yasiri <56668770+A454545@users.noreply.github.com>
2023-02-26 13:05:44 -0500

    Update Point.py

--- src/Point.py
+++ src/Point.py
@@ -1,5 +1,5 @@
 class Point:
-  def __init__(self, ID, lat, lon, time=None, mode=None):
+  def __init__(self, lat, lon, time=None, mode=None, ID=None):
     self.episodeID = ID
     self.lat = lat
     self.lon = lon
