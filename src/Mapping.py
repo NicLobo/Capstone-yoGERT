10110d2756f7f41a2c86c3bd97b2870a140003f1
Abeer Al-Yasiri <56668770+A454545@users.noreply.github.com>
2023-02-26 22:00:37 -0500

    added legend. Resolves #83

--- src/Mapping.py
+++ src/Mapping.py
@@ -13,6 +13,102 @@ from ActivityLocation import *
 from IPython.display import IFrame
 import folium
 from Point import * 
+from branca.element import Template, MacroElement
+
+# Global variables 
+template = """
+{% macro html(this, kwargs) %}
+
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.css"></link>
+
+  <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+  
+  <script>
+  $( function() {
+    $( "#maplegend" ).draggable({
+                    start: function (event, ui) {
+                        $(this).css({
+                            right: "auto",
+                            top: "auto",
+                            bottom: "auto"
+                        });
+                    }
+                });
+});
+
+  </script>
+</head>
+<body>
+
+ 
+<div id='maplegend' class='maplegend' 
+    style='position: absolute; z-index:9999; border:2px solid grey; background-color:rgba(255, 255, 255, 0.8);
+     border-radius:6px; padding: 10px; font-size:14px; right: 20px; bottom: 20px;'>
+     
+<div class='legend-title'>Legend</div>
+<div class='legend-scale'>
+  <ul class='legend-labels'>
+    <li><i class="fa-solid fa-circle-stop" style="font-size:20px;color:red;"></i>Stop</li>
+    <li><i class="fa-solid fa-car-side" style="font-size:20px;color:green;"></i>Drive</li>
+    <li><i class="fa-solid fa-person-walking" style="font-size:20px;color:blue;"></i>Walk</li>
+    <li><i class="fa-solid fa-circle-info" style="font-size:20px;color:orange;"></i>Activity Location</li>
+    <li><i class="fa fa-route" style="font-size:20px;color:green;"></i> Drive Route</li>
+    <li><i class="fa fa-route" style="font-size:20px;color:blue;"></i> Walk Route</li>
+    <li><i class="fa fa-route" style="font-size:20px;color:purple;"></i> Bike Route</li>
+  </ul>
+</div>
+</div>
+ 
+</body>
+</html>
+
+<style type='text/css'>
+  .maplegend .legend-title {
+    text-align: left;
+    margin-bottom: 5px;
+    font-weight: bold;
+    font-size: 90%;
+    }
+  .maplegend .legend-scale ul {
+    margin: 0;
+    margin-bottom: 5px;
+    padding: 0;
+    float: left;
+    list-style: none;
+    }
+  .maplegend .legend-scale ul li {
+    font-size: 80%;
+    list-style: none;
+    margin-left: 0;
+    line-height: 18px;
+    margin-bottom: 2px;
+    }
+  .maplegend ul.legend-labels li span {
+    display: block;
+    float: left;
+    height: 16px;
+    width: 30px;
+    margin-right: 5px;
+    margin-left: 0;
+    border: 1px solid #999;
+    }
+  .maplegend .legend-source {
+    font-size: 80%;
+    color: #777;
+    clear: both;
+    }
+  .maplegend a {
+    color: #777;
+    }
+</style>
+{% endmacro %}"""
 
 ## @brief This function maps a route and saves it as an interactive file.
 #  @param networkGraph NetworkGraph object that represents the base streats, roads, and walkways for the map.
@@ -53,6 +149,9 @@ def MapRoute(networkGraph, route, savePath):
     for i in range(1,len(routes)):
         route_map = ox.plot_route_folium(networkGraph.graph, routes[i], route_map=route_map, color=colorR, opacity=0.5)
     # place the legend
+    macro = MacroElement()
+    macro._template = Template(template)
+    route_map.get_root().add_child(macro)
     filepath = savePath
     route_map.save(filepath)
     IFrame(filepath, width=600, height=500)
@@ -72,6 +171,9 @@ def MapActivityLocation(activityLocations, stopPoints, savePath):
             base.add_child(folium.Marker([ACL.lat,ACL.lon], popup="Name:" + ACL.name + ", Amenity:" + ACL.amenity, 
                         tooltip=tooltip, icon=folium.Icon(color="orange", icon="info-sign")))
     #place legend
+    macro = MacroElement()
+    macro._template = Template(template)
+    base.get_root().add_child(macro)
     filepath = savePath
     base.save(filepath)
     IFrame(filepath, width=600, height=500)
@@ -95,6 +197,9 @@ def MapEpisodePoints(GPSCoords, savePath):
         base.add_child(folium.Marker([point.lat,point.lon], 
                         tooltip=tooltip, icon=folium.Icon(color=color, icon=icon, prefix="fa")))
     #place legend
+    macro = MacroElement()
+    macro._template = Template(template)
+    base.get_root().add_child(macro)
     base.save(savePath)
     IFrame(savePath, width=600, height=500)
 
