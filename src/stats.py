14806804bd69ef04db57423cc3a5da22b9895770
Niyatha Rangarajan <59780995+rangaran@users.noreply.github.com>
2023-03-06 12:19:44 -0500

    Fixed issues with trace0

--- src/stats.py
+++ src/stats.py
@@ -5,14 +5,18 @@ from Point import Point
 import os
 import pandas as pd  
 import glob
+from csv import writer
 
     
 def ping_frequency(trace): 
     filepath = trace
     
     files = glob.glob(filepath + "/*.csv")
+    print(files)
     for f in files:
-        df = df[["lat","long","time"]]
+        df = pd.read_csv(f)
+        #print(df.head(10))
+        
         df['time']= pd.to_datetime(df['time'])
 
 
@@ -20,48 +24,70 @@ def ping_frequency(trace):
         #Calculate stepsize using start and end time of dataset
         starttime = pd.to_datetime(df['time'].iloc[0])
         endtime = pd.to_datetime(df['time'].iloc[-1])
+        #print(starttime,endtime)
         totaltime = pd.Timedelta(endtime - starttime).seconds 
-
+        print(totaltime)
         if(totaltime == 0):
             stepsize = 1
         else:
             stepsize = (round(len(df)/totaltime)) 
-    t = pd.read_csv("stats.csv")  
-    t["Ping frequency"] = str(stepsize)  
-
+    
+    with open(trace+'/stats.csv', 'w') as f1:
+        writer_object = writer(f1)
+        writer_object.writerow(['Ping Frequency'])
+        writer_object.writerow([str(stepsize) ])
+  
 
 def mode_change(trace): 
 
     modes = []
 
-    c=0
-    changec = 0
-    filepath = trace+'./episodes'
-    files = glob.glob(filepath + "/*.csv")
 
+    changec = 0
+    filepath = trace+'/episode'
+    files = glob.glob(filepath + "/*.csv")
+    print(files)
+    filecount = 0
     for f in files:
+        data = csv.reader(open(f))
+        c = 0
+        
         for line in data:
             
             if c>0: 
-                data = csv.reader(open(f))
-                modes.append(line[8])
-                if modes[-1]!=modes[-2]:
-                    changec +=1
+                
+                modes.append(line[4])
+                #print(modes)
+                filecount+=1
+                if filecount >=2:
+                    if modes[-1]!=modes[-2]:
+                        changec +=1
+                    print(modes)
+                    print(changec)
+                break
             
             c = c+1
-    
-    t = pd.read_csv("stats.csv")  
-    t["Mode Change"] = str(changec)  
-
+    print(changec)
+    stats=trace+'/stats.csv'
+    t = pd.read_csv(stats)  
+    t.insert(1, column = "Mode Change", value = str(changec))  
+    t.to_csv(trace+"/stats.csv", index=False)
 
 
 def numberoftrips(trace): 
     count = 0
-    dir_path = trace+'./episodes'
+    dir_path = trace+'/episode'
     for path in os.scandir(dir_path):
         if path.is_file():
             count += 1
-    with open('stats.csv', 'w') as file:
-        pass
-    t = pd.read_csv("stats.csv")  
-    t["Number of trips"] = str(count)  
+   
+    print(count)
+    t = pd.read_csv(trace+"/stats.csv")  
+    
+    t.insert(1, column = "Number of trips", value = str(count))  
+    print(t.head(2))
+    t.to_csv(trace+"/stats.csv", index=False)
+
+ping_frequency('./trace')
+#numberoftrips('./trace')
+mode_change('./trace')
