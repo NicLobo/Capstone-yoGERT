cf380093aea1f1c2baca09d40dfd0b280b546c50
alyasira <alyasira@mcmaster.ca>
2023-03-06 22:21:34 -0500

    changed comments

--- test/NetworkGraphTesting/UnitTesting/test_NetworkGraph.py
+++ test/NetworkGraphTesting/UnitTesting/test_NetworkGraph.py
@@ -24,37 +24,37 @@ def test_CreateNetworkGraphFromTrace():
     NG = NetworkGraph(traceFilePath, "drive", False)
     assert type(NG.graph) == nx.classes.multidigraph.MultiDiGraph
 
-# Test 6.2.1.1
+# Test 6.2.1.2
 def test_CreateNetworkGraphFromEpisode():
     NG = NetworkGraph(episodeFilePath)
     assert type(NG.graph) == nx.classes.multidigraph.MultiDiGraph
 
-# Test 6.2.1.2
+# Test 6.2.1.3
 def test_GetNearestNode():
     NG = NetworkGraph(traceFilePath, "drive", False)
     nearestNode = NG.getNearestNode(GPSCoord)
     assert nearestNode in NG.graph
 
-# Test 6.2.1.3
+# Test 6.2.1.4
 def test_GetGraphMode():
     NG = NG = NetworkGraph(traceFilePath, "drive", False)
     mode = NG.getMode()
     assert mode == "drive"
 
-# Test 6.2.1.4
+# Test 6.2.1.5
 def test_GetNearestNodeBoundsException(capsys):
     NG = NetworkGraph(traceFilePath, "drive", False)
     NG.getNearestNode(OutGPSCoord)
     captured = capsys.readouterr()
     assert "OutOfBoundsCoordException" in captured.out
 
-# Test 6.2.1.5
+# Test 6.2.1.6
 def test_CreateNetworGraphModeException(capsys):
     NetworkGraph(traceFilePath, incorrectMode, False)
     captured = capsys.readouterr()
     assert "InvalidModeException" in captured.out
 
-# Test 6.2.1.6
+# Test 6.2.1.7
 def test_CreateNetworGraphFileException(capsys):
     NetworkGraph(emptyFilePath, "drive", False)
     captured = capsys.readouterr()
