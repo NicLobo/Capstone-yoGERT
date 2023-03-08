87dcab32f49773f98bc5b3ff947955746dd3c36c
Moksha <moksha.srinivas@gmail.com>
2023-03-08 11:27:26 -0500

    cleaned up preprocessing code

--- src/PreProcessing.py
+++ src/PreProcessing.py
@@ -10,7 +10,8 @@ import os
 
 #@brief this function confirms that CSV is valid and updates to correct column names, removing invalid data
 #@param csvpath - a full path to the input CSV file
-#@return Bool - if the data is valid -> true, if valid, the data is processed and written to a new file with the name of the original file concatenated with _processed
+#@param directoryname - directory that will be created to store processed traces
+#@return Bool - if the data is valid -> true, if valid, the data is processed and written to a new file within the specified directory
 def ValidateCSV(csvpath, directoryname):
     df = pd.read_csv(csvpath)
     try: 
@@ -32,16 +33,18 @@ def ValidateCSV(csvpath, directoryname):
             os.mkdir(directoryname)
             df = df.dropna(subset=['lat', 'long', 'time'])
 
+            #regular expressions must be updated
             dmslat = re.compile('/^[\+-]?(([1-8]?\d)\D+([1-5]?\d|60)\D+([1-5]?\d|60)(\.\d+)?|90\D+0\D+0)\D+[NSns]?$/')
             dmslong = re.compile('/^[\+-]?([1-7]?\d{1,2}\D+([1-5]?\d|60)\D+([1-5]?\d|60)(\.\d+)?|180\D+0\D+0)\D+[EWew]?$/')
 
             #convert to DD from DMS if DMS format detected
-            #print(df.iloc[0]['lat'])
-            #print(type(df.iloc[0]['lat']))
             if dmslat.match(str(df.iloc[0]['lat'])):
                 df['lat'] = df['lat'].apply(DMStoDD)
             if dmslong.match(str(df.iloc[0]['long'])):
                 df['long'] = df['long'].apply(DMStoDD)
+
+            df = df.astype({'lat':'float'})
+            df = df.astype({'long':'float'})
 
             #Invalid Latitude: max/min 90.0000000 to -90.0000000
             #Invalid Longitude: max/min 180.0000000 to -180.0000000
@@ -74,19 +77,13 @@ def ValidateCSV(csvpath, directoryname):
     except InvalidInputDataException: 
         raise Exception("InvalidInputDataException: invalid input, you do not have all required columns (latitude, longitude, time)") 
 
-
+#@brief this function converts DMS format lat/long to DD
+#@param dmsstring - a string containing the dms value
+#@return float with the correct dd representation
 def DMStoDD(dmsstring):
     deg, minutes, seconds, direction =  re.split('[°\'"]', dmsstring)
     return (float(deg) + float(minutes)/60 + float(seconds)/(60*60)) * (-1 if direction in ['W', 'S'] else 1)
 
-
-#ValidateCSV("/home/moksha/4G06/Capstone-yoGERT/src/exampleDataset.csv")
-
-#print(DMStoDD('78°55\'44.29458\"N'))
-#print(DMStoDD('124° 4\' 58\" W'))
-#print(DMStoDD('cookie'))
-
-ValidateCSV("/home/moksha/4G06/Capstone-yoGERT/src/exampleDataset/trace_1.csv", "testdir")
 
 
 
