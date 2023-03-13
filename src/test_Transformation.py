60701cfe9ac68c8246a6a90a49d8fc64e056d2d1
Niyatha Rangarajan <59780995+rangaran@users.noreply.github.com>
2023-03-13 14:32:18 -0400

    Basic test - exceptions

--- src/test_Transformation.py
+++ src/test_Transformation.py
@@ -9,21 +9,6 @@ import statistics
 from statistics import mode
 import glob
 import pandas as pd  
-from ActivityLocation import *
-from Transformation import *
-#trace file path
-
-tracepath = './trace/trace1/trace.csv'import csv
-import ast
-from datetime import datetime
-from Point import Point
-import os
-import pandas
-from csv import writer
-import statistics
-from statistics import mode
-import glob
-import pandas as pd  
 from ActivityLocation import ActivityLocation
 #
 # from Mapping import *
@@ -33,6 +18,7 @@ from Transformation import *
 tracepath = './trace/trace1/trace.csv'
 stoppath = './trace/trace1/stop/stops.csv'
 episodepath = './trace/trace1/episode/1_episode.csv'
+traceact = './trace/trace-activityLocation.csv'
 act1 = ActivityLocation.ActivityLocation('Laser Eye Center', 37.3154678,-121.9757438, 'doctors')
 actlist = ['Laser Eye Center', 37.3154678,-121.9757438, 'doctors']
 p1 = Point(37.3154678,-121.9757438)
@@ -55,21 +41,13 @@ def test_convertActivityLocation():
     assert convertActivityLocation([(p1,[act1])]) == [[p1.lat, p1.lon, [[act1.name,act1.lat,act1.lon, act1.amenity]]]]
 
 def test_convertActivityCSV():
-    
+    assert ((convertActivityCSV(traceact))[0]).lat == act1.lat
+    assert ((convertActivityCSV(traceact))[0]).lon == act1.lon
+    assert ((convertActivityCSV(traceact))[0]).name == act1.name
+    assert ((convertActivityCSV(traceact))[0]).amenity == act1.amenity
 
-stoppath = './trace/trace1/stop/stops.csv'
-episodepath = './trace/trace1/episode/1_episode.csv'
-def test_tracerelated(): 
-    assert type(tracerelated(tracepath)) == list
-    assert type(tracerelated(tracepath)[0]) == Point
-
-def test_stoprelated(): 
-    assert type(stoprelated(stoppath)) == list
-    assert type(stoprelated(stoppath)[0]) == Point
-
-def test_episoderelated(): 
-    assert type(episoderelated(episodepath)) == list
-    assert type(episoderelated(episodepath)[0]) == Point
-
-def test_summaryModeTrace():
-    assert type(summaryModeTrace(tracepath)) == str
+def test_convertListToActivityLocationObject():
+    assert (convertListToActivityLocationObject(actlist)).lat == act1.lat
+    assert (convertListToActivityLocationObject(actlist)).lon == act1.lon
+    assert (convertListToActivityLocationObject(actlist)).name == act1.name
+    assert (convertListToActivityLocationObject(actlist)).amenity == act1.amenity
