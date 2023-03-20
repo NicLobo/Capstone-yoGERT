a2fc9c2d3d78abba2bd83e2d03c2f06d515bda3c
yel16 <tommyylw0107@outlook.com>
2023-03-20 14:14:53 -0400

    Update Testing

---
+++ src/SSR_performance.py
@@ -0,0 +1,33 @@
+import main
+import ShortestRouteStop
+
+import timeit
+
+# Tested data
+traceFilePath = "/Users/tommylw/Desktop/University/SE_4G06/Capstone-yoGERT/src/trace/trace.csv"
+stopFilePath = "/Users/tommylw/Desktop/University/SE_4G06/Capstone-yoGERT/src/trace/stop/stops.csv"
+traceMode = "drive"
+episodeAnalysis = False
+
+# Test Case
+def performanceGSSR():
+    correctTime = 0
+    startTime = timeit.default_timer()
+    for i in range(10):
+        generatedShortestStopRoute = main.generateShortestStopRoute(traceFilePath, traceMode, stopFilePath)
+        if (type(generatedShortestStopRoute) == ShortestRouteStop.ShortestRouteStop):
+            correctTime +=1
+        
+        print("Complete One Time.")
+        i += 1
+    endTime = timeit.default_timer()
+
+    totalRunTime = endTime - startTime
+    averageRunTime = totalRunTime/10
+
+    # Report Result
+    print("10 cases run, " + str(correctTime) + "/10 cases passes.\nTotal runtime: " + str(totalRunTime) + " seconds.\nAverage runtime for 1 case: " + str(averageRunTime) + " seconds.")
+        
+# Automated Driver Code
+performanceGSSR()
+
