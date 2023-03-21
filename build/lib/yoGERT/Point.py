c85d037fb8f3387dc95a304f55c98ceb708d76cb
Moksha Srinivasan <moksha.srinivas@gmail.com>
2023-03-21 03:25:07 -0400

    deleting build files

--- build/lib/yoGERT/Point.py
+++
@@ -1,7 +0,0 @@
-class Point:
-  def __init__(self, lat, lon, time=None, mode=None, ID=None):
-    self.episodeID = ID
-    self.lat = lat
-    self.lon = lon
-    self.time = time
-    self.mode = mode
--- build/lib/yoGERT/SHPtoNetworkDataset.py
+++
@@ -1,7 +0,0 @@
-"""! @brief Converts CSVs to point SHPs."""
-
-"""@package src
-
-This module will process incoming travel episode point SHP files 
-and convert them to the open source equivalent network dataset types 
-"""
